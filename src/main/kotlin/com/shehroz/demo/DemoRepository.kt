72e73d25d1c21fcb79f4954ae6e26a14b911c58c
Shehroz Ali <sshehroza13@gmail.com>
2023-05-29 15:52:21 +0500

    Created new Repository interface Implemented JpaRepository<DemoEntity, UUID> Added @Repository

---
+++ src/main/kotlin/com/shehroz/demo/DemoRepository.kt
@@ -0,0 +1,9 @@
+package com.shehroz.demo
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface DemoRepository: JpaRepository<DemoEntity, UUID> {
+}
