cc24ad53088777d0c0b717ac40e455d019ab4d7e
Shehroz Ali <sshehroza13@gmail.com>
2023-05-29 14:42:30 +0500

    Added MockMvc and ObjectMapper dependency

--- src/test/kotlin/com/shehroz/demo/DemoControllerTest.kt
+++ src/test/kotlin/com/shehroz/demo/DemoControllerTest.kt
@@ -1,4 +1,17 @@
 package com.shehroz.demo
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+
+@WebMvcTest(controllers = [DemoController::class])
+@ExtendWith(SpringExtension::class)
 class DemoControllerTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
 }
