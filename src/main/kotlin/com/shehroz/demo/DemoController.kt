b5a0288825c6965d8d6ed230a9e241d27f6f3a36
Shehroz Ali <sshehroza13@gmail.com>
2023-05-29 14:09:15 +0500

    getAllUsers: Created new method to get all users

--- src/main/kotlin/com/shehroz/demo/DemoController.kt
+++ src/main/kotlin/com/shehroz/demo/DemoController.kt
@@ -1,9 +1,22 @@
 package com.shehroz.demo
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("api/v1")
 class DemoController {
+
+    @GetMapping("/users/all")
+    fun getAllUsers(): Mono<ResponseEntity<String>> {
+        return Mono
+            .just(
+                ResponseEntity
+                    .ok()
+                    .body("Hello Kotlin Spring WebFlux")
+            )
+    }
 }
