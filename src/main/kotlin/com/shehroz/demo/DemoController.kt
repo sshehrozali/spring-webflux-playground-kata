597a9354c05527bd7271d3f5f98df44e29041eb8
Shehroz Ali <sshehroza13@gmail.com>
2023-05-29 14:05:21 +0500

    Created new class file Added @RestController Added @RequestMapping

---
+++ src/main/kotlin/com/shehroz/demo/DemoController.kt
@@ -0,0 +1,9 @@
+package com.shehroz.demo
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1")
+class DemoController {
+}
