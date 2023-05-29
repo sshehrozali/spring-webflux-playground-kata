82b55ef8d2538d97c9d8c79d3dcdbc8ba3c8af55
Shehroz Ali <sshehroza13@gmail.com>
2023-05-29 14:54:54 +0500

    Implemented test to test /api/v1/users/all

--- src/test/kotlin/com/shehroz/demo/DemoControllerTest.kt
+++ src/test/kotlin/com/shehroz/demo/DemoControllerTest.kt
@@ -1,17 +1,32 @@
 package com.shehroz.demo
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
 
 @WebMvcTest(controllers = [DemoController::class])
 @ExtendWith(SpringExtension::class)
 class DemoControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
+
     @Autowired
     private lateinit var objectMapper: ObjectMapper
+
+    @Test
+    @DisplayName("Should Get All Users")
+    fun shouldGetAllUsers() {
+        mockMvc.perform(get("/api/v1/users/all"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .asyncResult
+    }
 }
