175219b4682f5d76f17cc3acfb39a1580894590a
Shehroz Ali <sshehroza13@gmail.com>
2023-05-29 15:27:44 +0500

    Implemented entity

---
+++ src/main/kotlin/com/shehroz/demo/DemoEntity.kt
@@ -0,0 +1,20 @@
+package com.shehroz.demo
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "users")
+class DemoEntity(
+    @Id
+    var id: UUID = UUID.randomUUID(),
+
+    @Column(name = "user_name", nullable = false)
+    var userName: String,
+
+    @Column(name = "phone_number", nullable = true)
+    var phoneNumber: Long
+) : java.io.Serializable
