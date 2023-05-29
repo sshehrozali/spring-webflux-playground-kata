773545a5abc1ec4fdf0e55bd2e425402b412b619
Shehroz Ali <sshehroza13@gmail.com>
2023-05-29 15:12:07 +0500

    Refactored Test passed

--- src/test/kotlin/com/shehroz/demo/DemoControllerTest.kt
+++ src/test/kotlin/com/shehroz/demo/DemoControllerTest.kt
@@ -10,23 +10,28 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.assertj.core.api.Assertions.assertThat
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
 
 
-@WebMvcTest(controllers = [DemoController::class])
+@WebFluxTest(controllers = [DemoController::class])
 @ExtendWith(SpringExtension::class)
 class DemoControllerTest {
     @Autowired
-    private lateinit var mockMvc: MockMvc
-
-    @Autowired
-    private lateinit var objectMapper: ObjectMapper
+    private lateinit var webTestClient: WebTestClient
 
     @Test
     @DisplayName("Should Get All Users")
     fun shouldGetAllUsers() {
-        mockMvc.perform(get("/api/v1/users/all"))
-            .andExpect(status().isOk())
-            .andReturn()
-            .asyncResult
+        // Arrange, Act, Assert
+        webTestClient
+            .get()
+            .uri("/api/v1/users/all")
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody<String>()
+            .isEqualTo("Hello Kotlin Spring WebFlux")
     }
 }
