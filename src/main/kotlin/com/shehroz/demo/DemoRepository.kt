21a00b8ad64ba74e4416386071acf8e3f4c9547d
Shehroz Ali <sshehroza13@gmail.com>
2023-06-07 15:16:02 +0500

    Created new JPA query Wrapped with Optional

--- src/main/kotlin/com/shehroz/demo/DemoRepository.kt
+++ src/main/kotlin/com/shehroz/demo/DemoRepository.kt
@@ -2,9 +2,10 @@ package com.shehroz.demo
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 import java.util.UUID
 
 @Repository
 interface DemoRepository : JpaRepository<User, UUID> {
-    fun findByUserId(userId: UUID): User
+    fun findByUserId(userId: UUID): Optional<User>
 }
