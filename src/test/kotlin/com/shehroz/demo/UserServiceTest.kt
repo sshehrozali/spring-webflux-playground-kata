28a739ef344446269ed229ee3a243bbafd91405d
Shehroz Ali <sshehroza13@gmail.com>
2023-06-07 16:29:08 +0500

    Test passed

--- src/test/kotlin/com/shehroz/demo/UserServiceTest.kt
+++ src/test/kotlin/com/shehroz/demo/UserServiceTest.kt
@@ -2,9 +2,12 @@ package com.shehroz.demo
 
 import io.mockk.every
 import io.mockk.mockk
+import org.assertj.core.api.Assertions.anyOf
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import java.util.*
 
 class UserServiceTest {
@@ -35,6 +38,31 @@ class UserServiceTest {
 
         // Act
         val actual = serviceUnderTest.getAllUsers()
+
+        // Assert
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    @DisplayName("Should Get User Successfully by UUID")
+    fun shouldGetUserSuccessfullyByUUID() {
+        // Arrange
+        val savedUserId = UUID.randomUUID()
+        val savedUser = User(
+            UUID.randomUUID(),
+            savedUserId,
+            "Shehroz",
+            3352669779
+        )
+        every { userRepository.findByUserId(any()) } returns Optional.of(savedUser)
+
+        val expected = UserDTO(
+            "Shehroz",
+            3352669779
+        )
+
+        // Act
+        val actual = serviceUnderTest.getUserByUUID(savedUserId)
 
         // Assert
         assertThat(actual).isEqualTo(expected)
