b3383daf1440a63313cbfb79ba69d03ea9629160
Shehroz Ali <sshehroza13@gmail.com>
2023-07-14 14:50:30 +0500

    Refactored test names

--- src/test/kotlin/com/shehroz/demo/UserServiceTest.kt
+++ src/test/kotlin/com/shehroz/demo/UserServiceTest.kt
@@ -45,8 +45,7 @@ class UserServiceTest {
     }
 
     @Test
-    @DisplayName("Should Get A User Successfully by UUID")
-    fun shouldGetAUserSuccessfullyByUUID() {
+    fun `should get a user successfully by valid UUID if user exists`() {
         val savedUserId = UUID.randomUUID()
         val savedUser = User(
             UUID.randomUUID(),
@@ -64,8 +63,7 @@ class UserServiceTest {
     }
 
     @Test
-    @DisplayName("Should Throw Exception If No User Found By UUID")
-    fun shouldThrowExceptionIfNoUserFoundByUUID() {
+    fun `should throw UserNotFoundException if no user found by UUID`() {
         val savedUserId = UUID.randomUUID()
         every { userRepository.findByUserId(savedUserId) } returns Optional.empty()
 
@@ -76,8 +74,7 @@ class UserServiceTest {
     }
 
     @Test
-    @DisplayName("Should Throw Exception If UUID Passed Is Invalid")
-    fun shouldThrowExceptionIfUUIDPassedIsInvalid() {
+    fun `should throw InvalidUUIDException if UUID passed is invalid`() {
         val result = serviceUnderTest.getUserByUUID(UUID.fromString("00000000-0000-0000-0000-000000000000"))
         StepVerifier.create(result)
             .expectError<InvalidUUIDException>()
